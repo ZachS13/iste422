41f4e62842cb85debe18f07d6aa19feb862f46b2
zachs1326 <zachs1326@gmail.com>
2024-01-31 17:57:58 -0500

    adding personInformation to exercise 2 also

---
+++ exercise2/personInformation.java
@@ -0,0 +1,35 @@
+package exercise2;
+
+public class personInformation {
+    private String name;
+    private String email;
+    private String city;
+    private String mac;
+    private String timestamp;
+    private String creditcard;
+
+    /**
+     * A person can contain all of the following items.
+     * @param name - Required
+     * @param email
+     * @param city
+     * @param mac
+     * @param timestamp
+     * @param creditcard - Required for csv
+     */
+    public personInformation(String name, String email, String city, String mac, String timestamp, String creditcard) {
+        this.name = name;
+        this.email = email;
+        this.city = city;
+        this.mac = mac;
+        this.timestamp = timestamp;
+        this.creditcard = creditcard;
+    }
+
+    public String getName() { return this.name; }
+    public String getEmail()  {return this.email; }
+    public String getCity() { return this.city; }
+    public String getMac() { return this.mac; }
+    public String getTimestamp() { return this.timestamp; }
+    public String getCreditcard() { return this.creditcard; }
+}
