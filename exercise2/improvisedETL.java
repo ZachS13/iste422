6602b7c025029a6befd8bad484fc5b63955046d4
zachs1326 <zachs1326@gmail.com>
2024-02-01 20:55:01 -0500

    finished the exercise and everything is working finally

--- exercise2/improvisedETL.java
+++ exercise2/improvisedETL.java
@@ -1,9 +1,16 @@
 package exercise2;
+
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class improvisedETL {
     
@@ -12,6 +19,7 @@ public class improvisedETL {
 
     public improvisedETL(String filename) {
         this.filename = filename;
+        this.peopleList = new ArrayList<>();
         completeExercise();
     }
 
@@ -29,6 +37,7 @@ public class improvisedETL {
         boolean written = false;
         String newFilename = getNewFilename();
         newFilename += ".csv";
+        newFilename = "exercise2/" + newFilename;
 
         try {
             // creates the new file
@@ -38,7 +47,7 @@ public class improvisedETL {
                 // if there is a name and creditcard
                 String name = info.getName();
                 String card = info.getCreditcard();
-                if (!name.equals("") && !card.equals("null")) {
+                if (!name.equals("") && card != null) {
                     // write in the file name,creditcard
                     String line = name + "," + card;
                     writer.println(line);
@@ -56,6 +65,28 @@ public class improvisedETL {
     private boolean readData() {
         boolean read = false;
         // read the file here
+        try {
+            FileReader file = new FileReader(this.filename);
+            JSONParser parser = new JSONParser();
+    
+            JSONArray arr = (JSONArray) parser.parse(file);
+            for(Object per : arr) {
+                JSONObject person = (JSONObject) per;
+                String name = (String) person.get("name");
+                String email = (String) person.get("email");
+                String city = (String) person.get("city");
+                String mac = (String) person.get("mac");
+                String timestamp = (String) person.get("timestamp");
+                String creditcard = (String) person.get("creditcard");
+                
+                personInformation info = new personInformation(name, email, city, mac, timestamp, creditcard);
+                this.peopleList.add(info);
+            }
+
+        } catch (IOException | ParseException e) {
+            System.out.println(e);
+        }
+
         return read;
     }
 
@@ -67,6 +98,6 @@ public class improvisedETL {
     }
 
     public static void main(String[] args) {
-        new improvisedETL("data.json");
+        new improvisedETL("exercise2/data.json");
     }
 }
